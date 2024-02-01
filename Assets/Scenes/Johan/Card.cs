e8aac35eb8e6b0ba54738aa1838e92e136cca6a2
Eggnrice <johanbello2002@hotmail.com>
2024-02-01 12:10:38 -0500

    Changes

--- Assets/Scenes/Johan/Card.cs
+++ Assets/Scenes/Johan/Card.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
 
 public class Card : MonoBehaviour
 {
-    
+    public int number;
+    public string type;
+
+
+    //private void OnEnable()
+    //{
+    //    GameManager.instance.onCardsPlayed += ;
+    //}
+
+    //private void OnDisable()
+    //{
+    //    onCardsPlayed -= DrawCard;
+    //}
 
 
 }
