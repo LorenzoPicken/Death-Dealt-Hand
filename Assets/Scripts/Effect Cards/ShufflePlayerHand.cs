2384abed74505869f3e2010c32623b48aed08ebe
LorenzoP004 <lorenzopicken@gmail.com>
2024-03-15 10:35:17 -0400

    Add new Shuffle hand effect.

--- Assets/Scripts/Effect Cards/ShufflePlayerHand.cs
+++ Assets/Scripts/Effect Cards/ShufflePlayerHand.cs
@@ -6,6 +6,7 @@ public class ShufflePlayerHand : EffectCard
 {
     [SerializeField] PlayerStateManager player;
     [SerializeField] AIBehaviour enemy;
+    
     public override void Draw()
     {
         base.Draw();
@@ -19,13 +20,71 @@ public class ShufflePlayerHand : EffectCard
 
     public override void Execute()
     {
+        int cardsInHand = 0;
         if (GAMEMANAGER.Instance.currentRoundState == RoundState.PLAYERTURN)
         {
-            
+            foreach(Card card in player.playerCards)
+            {
+                //Visually Send Card Back To Deck
+            }
+            cardsInHand = player.playerCards.Count;
+            for(int i = 0; i < cardsInHand; i++)
+            {
+                player.playerCards[0].InHand = false;
+                GAMEMANAGER.Instance.deck.Add(player.playerCards[0]);
+                player.playerCards.Remove(player.playerCards[0]);
+                
+            }
+
+            //Reshuffle Deck Visualy
+            GAMEMANAGER.Instance.deck = GAMEMANAGER.Instance.Shuffle(GAMEMANAGER.Instance.deck);
+
+            foreach(Transform playerSlot in GAMEMANAGER.Instance.playerSlots)
+            {
+                GAMEMANAGER.Instance.deck[0].transform.position = playerSlot.transform.position;
+                GAMEMANAGER.Instance.deck[0].transform.rotation = playerSlot.transform.rotation;
+                GAMEMANAGER.Instance.deck[0].inHand = true;
+                GAMEMANAGER.Instance.deck.Remove(GAMEMANAGER.Instance.deck[0]);
+
+
+                //Replace Card Teleport With Animation
+            }
+
+
+
         }
         else
         {
-            
+            foreach (Card card in enemy.handList)
+            {
+                //Visually Send Card Back To Deck
+            }
+            cardsInHand = enemy.handList.Count;
+
+            for (int i = 0; i < cardsInHand; i++)
+            {
+                
+                GAMEMANAGER.Instance.deck.Add(enemy.handList[0]);
+                enemy.handList.Remove(enemy.handList[0]);
+
+            }
+
+            //Reshuffle Deck Visualy
+            GAMEMANAGER.Instance.deck = GAMEMANAGER.Instance.Shuffle(GAMEMANAGER.Instance.deck);
+
+
+            foreach (Transform playerSlot in GAMEMANAGER.Instance.playerSlots)
+            {
+                GAMEMANAGER.Instance.deck[0].transform.position = playerSlot.transform.position;
+                GAMEMANAGER.Instance.deck[0].transform.rotation = playerSlot.transform.rotation;
+                
+                GAMEMANAGER.Instance.deck.Remove(GAMEMANAGER.Instance.deck[0]);
+
+
+                //Replace Card Teleport With Animation
+            }
         }
     }
+
+    
 }
